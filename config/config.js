eebec7c692b39938b36fd25e2cf4a7f86dec6d59
Julio Osorio <juliodion_12345@hotmail.com>
2016-01-04 14:26:00 -0600

    added rabbitmq for sending evaluation and operations to queue

--- config/config.js
+++ config/config.js
@@ -6,7 +6,11 @@ var config = {
     servicePort: process.env.SERVICE_PORT,
     utilityHost: process.env.UTILITY_HOST,
     utilityPort: process.env.UTILITY_PORT,
-    utilityProtocol: process.env.UTILITY_PROTOCOL
+    utilityProtocol: process.env.UTILITY_PROTOCOL,
+    queueHost: process.env.QUEUE_HOST,
+    queuePort: process.env.QUEUE_PORT,
+    queueName: process.env.QUEUE_NAME,
+    queueProtocol: process.env.QUEUE_PROTOCOL
   }
 };
 
