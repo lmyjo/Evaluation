b4bac566e16e321ebeb5cc3024b902db85fcda47
Julio Osorio <juliodion_12345@hotmail.com>
2016-02-08 18:00:35 -0600

    Removed opened port 5672

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -d -p 5672:5672 --hostname rabbitmq --name investmentsqueue rabbitmq
+docker run -d --hostname rabbitmq --name investmentsqueue rabbitmq
 
 docker run -p 3001:3000 -d --name evaluation.investments --link utility.investments:utility.investments --link investmentsqueue:rabbitmq --env-file=docker/env lmyjo/evaluation
